bc78b3378852ca9d796af776d14a2928adf261bb
QubeetChannel <QubeetChannel@gmail.com>
2023-03-01 12:24:41 +0300

    Sharp

--- Program.cs
+++ Program.cs
@@ -23,9 +23,9 @@ namespace C_Sharp
 
             for (int i = 0; i < str.Length; i++) { Str.Remove(CharsEN[i]); }
             char[] Symbols = Str.ToCharArray();
-            for (int i = 0; i < CharsEN.Length; i++)
+            for (int i = 0; i < CharsEN.Length; i++) { }
 
-                    return true;
+            return true;
         }
 
 
